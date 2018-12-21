08c9bc052b96d8865057d4ce607c0ecd074f4a02
Cem Pesket <pesketcem@gmail.com>
2018-12-21 16:30:01 +0300

    site under construction update

--- src/App.js
+++ src/App.js
@@ -9,18 +9,24 @@ class App extends Component {
 
     render() {
         return (
+            // <div className="App">
+            //     <SearchBar/>
+            //     <div className="row">
+            //         <div className="col-md-8">
+            //             <VideoDetail/>
+
+            //         </div>
+            //         <div className="col-md-4">
+            //             <VideoList/>
+            //         </div>
+            //     </div>
+
+            // </div>
             <div className="App">
-                <SearchBar/>
-                <div className="row">
-                    <div className="col-md-8">
-                        <VideoDetail/>
-
-                    </div>
-                    <div className="col-md-4">
-                        <VideoList/>
-                    </div>
-                </div>
-
+                <h1>
+                Site under construction
+                </h1>
+                <p>soon to be opened</p>
             </div>
         );
     }
